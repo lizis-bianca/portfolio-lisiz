6d31cb19d8103b15736ce770bceb0524f7e12105
lisiz <91273140+lizis-bianca@users.noreply.github.com>
2025-03-12 19:45:02 -0300

    Refactor modeToggle component; remove unused imports and enhance photo component styles for improved dark mode support

--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -6,11 +6,11 @@ export default function Photo() {
     return (
     <div className="w-full h-full relative">
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2, duration: 0.4, ease: 'easeIn'  } }}>
-            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut'  } }} className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut'  } }} className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] dark:mix-blend-lighten absolute">
                 <Image src="/assets/avatar.png" priority quality={100} fill alt="" className="object-contain rounded-full" />
             </motion.div>
 
-            <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]" fill="transparent" viewBox="0 0 520 520" xmlns="http://www.w3.org/2000/svg">
+            <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px] z-50" fill="transparent" viewBox="0 0 520 520" xmlns="http://www.w3.org/2000/svg">
                 <motion.circle cx="253" cy="253" r="250" stroke="#c66828" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" 
                 initial={{strokeDasharray: "24 10 0 0"}}
                 animate={{
