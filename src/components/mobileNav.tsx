35e19757ed9c30243ccd7c0ac37860a17297a491
lisiz <91273140+lizis-bianca@users.noreply.github.com>
2025-09-20 14:24:30 -0300

    :bulb: comment route `/services`

--- src/components/mobileNav.tsx
+++ src/components/mobileNav.tsx
@@ -8,7 +8,7 @@ import { useTranslations } from "next-intl"
 
 const links = [
     { name: 'home', path: '/' },
-    { name: 'services', path: '/services' },
+    // { name: 'services', path: '/services' },
     { name: 'resume', path: '/resume' },
     { name: 'work', path: '/work' },
     { name: 'contact', path: '/contact' },
